6d505a46019e0af95b477273857e139cf6e09ca8
Luke Aldover <122496790+Laldover04@users.noreply.github.com>
2024-12-23 11:49:32 -0700

    Finished game
    different zombie movement than the first, needs a little work.
    added a zombie counter at the top left.

--- lawn.js
+++ lawn.js
@@ -4,15 +4,15 @@ let rows = 16;
 let columns = 16;
 
 let board;
-let boardWidth = tileSize * columns;
-let boardHeight = tileSize * rows;
+let boardWidth = tileSize * columns; // 32 * 16
+let boardHeight = tileSize * rows; // 32 * 16
 let context;
 
 //plant
-let plantWidth = tileSize; // 64px
-let plantHeight = tileSize; // 32 px
-let plantX = tileSize * columns / 2 - tileSize;
-let plantY = boardHeight - tileSize * 3;
+let plantWidth = tileSize;
+let plantHeight = tileSize;
+let plantX = tileSize * columns/2 - tileSize;
+let plantY = tileSize * rows - tileSize*2;
 
 let plant = {
     x : plantX,
@@ -22,30 +22,37 @@ let plant = {
 }
 
 let plantImg;
-let plantVelocityX = tileSize;
+let plantVelocityX = tileSize; //plant moving speed
 
- // zombies
- let zombieArray = [];
- let zombieWidth = tileSize; // 32px
- let zombieHeight = tileSize * 2; // 64px
- let zombieX = tileSize;
- let zombieY = tileSize;
+//zombies
+let zombieArray = [];
+let zombieWidth = tileSize;
+let zombieHeight = tileSize*2;
+let zombieX = tileSize;
+let zombieY = tileSize;
+let zombieImg;
 
- let zombieRows = 2;
- let zombieColumns = 3;
- let zombieCount = 0;
- let zombieVelocity = 1;
+let zombieRows = 2;
+let zombieColumns = 3;
+let zombieCount = 0; //number of zombies to defeat
+let zombieVelocityX = 1; //zombie moving speed
 
+let zombieImgArray = [];
+
+//peas
+let peaArray = [];
+let peaVelocityY = -10; //pea moving speed
+
+let score = 0;
+let gameOver = false;
 
 window.onload = function() {
     board = document.getElementById("board");
     board.width = boardWidth;
     board.height = boardHeight;
-    context = board.getContext("2d");
-    
-    // draw initial plant
-    context.fillStyle="green";
-    context.fillRect(plant.x, plant.y, plant.width, plant.height);
+    context = board.getContext("2d"); //used for drawing on the board
+
+    //draw initial plant
 
     //load images
     plantImg = new Image();
@@ -56,14 +63,26 @@ window.onload = function() {
 
     zombieImg = new Image();
     zombieImg.src = "./zombie1.png";
-    createZombies();
-    
+    zombieImgArray.push(zombieImg);
+    zombieImg = new Image();
+    zombieImg.src = "./zombie2.png";
+    zombieImgArray.push(zombieImg);
+    zombieImg = new Image();
+    zombieImg.src = "./zombie3.png";
+    zombieImgArray.push(zombieImg);
+    createzombies();
+
     requestAnimationFrame(update);
     document.addEventListener("keydown", moveplant);
+    document.addEventListener("keyup", shoot);
 }
 
 function update() {
     requestAnimationFrame(update);
+
+    if (gameOver) {
+        return;
+    }
 
     context.clearRect(0, 0, board.width, board.height);
 
@@ -71,40 +90,140 @@ function update() {
     context.drawImage(plantImg, plant.x, plant.y, plant.width, plant.height);
 
     //zombie
-    for(let i = 0; i < zombieArray.length; i++){
+    moveZombies();
+
+    //peas
+    movePeas();
+
+    //next level
+    if (zombieCount == 0) {
+        //increase the number of zombies in columns and rows by 1
+        score += zombieColumns * zombieRows * 100; //bonus points :)
+        zombieColumns = Math.min(zombieColumns + 1, columns/2 -2); //cap at 16/2 -2 = 6
+        zombieRows = Math.min(zombieRows + 1, rows-4);  //cap at 16-4 = 12
+        if (zombieVelocityX > 0) {
+            zombieVelocityX += 0.2; //increase the zombie movement speed towards the right
+        }
+        else {
+            zombieVelocityX -= 0.2; //increase the zombie movement speed towards the left
+        }
+        zombieArray = [];
+        peaArray = [];
+        createzombies();
+    }
+
+    //score
+    context.fillStyle="white";
+    context.font="16px courier";
+    context.fillText("Score:" + score + "\n Zombies:" + zombieCount, 5, 20);
+}
+
+function moveZombies(){
+    for (let i = 0; i < zombieArray.length; i++) {
         let zombie = zombieArray[i];
-        if(zombie.alive){
-            zombie.x += zombieVelocity;
+        if (zombie.alive) {
+            let randomNum = Math.floor(Math.random() * 3)
+            if(randomNum > 1)
+             zombie.y += randomNum - 1;
+
+            //random movement left or right
+            zombie.x += (zombieVelocityX - randomNum)
+
+            //if zombie touches the borders
+            if (zombie.x + zombie.width >= board.width) {
+                zombie.x -= zombieVelocityX;
+            } else if (zombie.x <= 0) {
+                zombie.x += zombieVelocityX;
+            }
             context.drawImage(zombie.img, zombie.x, zombie.y, zombie.width, zombie.height);
 
+            if (zombie.y >= plant.y) {
+                gameOver = true;
+            }
         }
+    }
+}
+
+function movePeas(){
+    for (let i = 0; i < peaArray.length; i++) {
+        let pea = peaArray[i];
+        pea.y += peaVelocityY;
+        context.fillStyle="green";
+        context.fillRect(pea.x, pea.y, pea.width, pea.height);
+
+        //pea collision with zombies
+        for (let j = 0; j < zombieArray.length; j++) {
+            let zombie = zombieArray[j];
+            if (!pea.used && zombie.alive && detectCollision(pea, zombie)) {
+                pea.used = true;
+                zombie.alive = false;
+                zombieCount--;
+                score += 100;
+            }
+        }
+    }
+
+    //clear peas
+    while (peaArray.length > 0 && (peaArray[0].used || peaArray[0].y < 0)) {
+        peaArray.shift(); //removes the first element of the array
     }
 }
 
 function moveplant(e) {
-    
-    if (e.code == "ArrowRight" && plant.x + plant.width + plantVelocityX <= board.width) {
-        plant.x += plantVelocityX;
-    } else if ( e.code == "ArrowLeft" && plant.x - plantVelocityX >= 0) {
-        plant.x -= plantVelocityX;
+    if (gameOver) {
+        return;
     }
-    
+
+    if (e.code == "ArrowLeft" && plant.x - plantVelocityX >= 0) {
+        plant.x -= plantVelocityX; //move left one tile
+    }
+    else if (e.code == "ArrowRight" && plant.x + plantVelocityX + plant.width <= board.width) {
+        plant.x += plantVelocityX; //move right one tile
+    }
 }
 
-function createZombies() {
-
-        for (let r = 0; r < zombieRows; r++) {
-            for (let c = 0; c < zombieColumns; c++) {
+function createzombies() {
+    for (let r = 0; r < zombieRows; r++) {
+        for (let c = 0; c < zombieColumns; c++) {
+                let randomNum = Math.floor(Math.random() * 4)
+                if(randomNum < 3){
                     let zombie = {
-                            img : zombieImg,
-                            x : zombieX + c*zombieWidth,
-                            y : zombieY + r*zombieHeight,
-                            width : zombieWidth,
-                            height : zombieHeight,
-                            alive : true
+                        img : zombieImgArray[randomNum],
+                        x : zombieX + c*zombieWidth,
+                        y : zombieY - (r + 1)*zombieHeight,
+                        width : zombieWidth,
+                        height : zombieHeight,
+                        alive : true
                     }
-                zombieArray.push(zombie)
-            }
+                    zombieArray.push(zombie)
+                }
+            
         }
-        zombieCount = zombieArray.length;
+    }
+    zombieCount = zombieArray.length;
+}
+
+function shoot(e) {
+    if (gameOver) {
+        return;
+    }
+
+    if (e.code == "Space") {
+        //shoot
+        let pea = {
+            x : plant.x + plantWidth*15/32,
+            y : plant.y,
+            width : tileSize/4,
+            height : tileSize/4,
+            used : false
+        }
+        peaArray.push(pea);
+    }
+}
+
+function detectCollision(a, b) {
+    return a.x < b.x + b.width &&   //a's top left corner doesn't reach b's top right corner
+           a.x + a.width > b.x &&   //a's top right corner passes b's top left corner
+           a.y < b.y + b.height &&  //a's top left corner doesn't reach b's bottom left corner
+           a.y + a.height > b.y;    //a's bottom left corner passes b's top left corner
 }
